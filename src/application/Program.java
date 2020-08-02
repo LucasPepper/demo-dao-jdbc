c7a72c133d562a0093db4a7c3dbff70a7a54321d
lucasifnmg <pimentalucas@hotmail.com>
2020-08-02 16:03:54 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,8 +10,8 @@ public class Program {
         // A factory vai instanciar o novo DAO, sem expor ele para o restante do programa
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: Seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
     }
 }
